3c9019ef0a2c2aad80b58d75d705ed2f252ab148
seoyeon08 <silversun0808@gmail.com>
2020-09-27 22:19:39 +0900

    LoadingActivity 추가(사진 미포함), 2초딜레이 기능 완료

---
+++ app/src/main/java/com/example/forsolo/LoadingActivity.java
@@ -0,0 +1,27 @@
+package com.example.forsolo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        getSupportActionBar().hide(); // 해당 화면 액션봐 제거
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(LoadingActivity.this,MainActivity.class));
+                finish();
+            }
+        }, 2000); // 2초 딜레이
+    }
+}
