f9dd3a15c08ebdfe1c11a5b3c21ec3698d30b045
seoyeon08 <silversun0808@gmail.com>
2020-09-27 22:09:53 +0900

    Commit Init

---
+++ app/src/main/java/com/example/forsolo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.forsolo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//    여기서 문제 빨간색은 뭐였지?!연동은 도어요 !오 정답 !
+    }
+}
