69c57d2d70f347ba27fb6335d631981f1568fb08
seoyeon08 <silversun0808@gmail.com>
2020-10-06 19:38:52 +0900

    JoinActivity 구현중.

--- app/src/main/java/com/example/forsolo/JoinActivity.java
+++ app/src/main/java/com/example/forsolo/JoinActivity.java
@@ -1,20 +1,34 @@
 package com.example.forsolo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class JoinActivity extends AppCompatActivity {
 
     private TextView btn_backLogin;
+    private FirebaseAuth mAuth; //파이어베이스 관련 변수 선언
+    private static final String TAG = "Start Activity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
 
         btn_backLogin = findViewById(R.id.btn_backLogin);
         btn_backLogin.setOnClickListener(new View.OnClickListener() {
@@ -25,4 +39,42 @@ public class JoinActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            switch (view.getId()){
+
+            }
+        }
+    }
+
+    private void signUp(){
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            //UI
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            //UI
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
