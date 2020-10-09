c2487fc993f5cd94e3757ee23d529286f25ea8f7
seoyeon08 <silversun0808@gmail.com>
2020-10-10 02:10:39 +0900

    report 기능 완료, login layout 간격 수정

--- app/src/main/java/com/example/forsolo/LoginActivity.java
+++ app/src/main/java/com/example/forsolo/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
